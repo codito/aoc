37ffb0020b907e554a6b463d19ed5262c31b2690
Arun Mahapatra <codito@codito.in>
2022-08-07 18:17:29 +0530

    chore: add 2015.1.

--- packages/2015/src/1.ts
+++ packages/2015/src/1.ts
@@ -1,1 +1,21 @@
-export const sayHello = () => "hello";
+export function findFloor(input: string): [number, number] {
+  let position = 0;
+  let basement = -1;
+  for (let i = 0; i < input.length; i++) {
+    position += input[i] === "(" ? 1 : -1;
+                        
+    if (basement < 0 && position === -1) {
+      basement =  i;
+    }
+  }
+
+  return [position, basement];
+}
+
+export function findFloor2(input: string): number {
+  return Array.from(input)
+    .reduce<number>((prev, current) => {
+      //console.log(`${prev} ${current} ${currentIndex} ${array}`);
+      return current === "(" ? prev + 1 : prev - 1;
+    }, 0);
+}
